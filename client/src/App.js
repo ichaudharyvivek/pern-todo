91be5f4198b067f79aca5ed233af6e40607d6f55
Vivek Chaudhary <vkc369258@gmail.com>
2023-09-04 14:52:42 +0000

    Update App.js and index.html

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import React, { Fragment } from 'react';
 import './App.css';
 
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <h1>Hello World!</h1>
+    </Fragment>
+  );
 }
 
 export default App;
