a2d1fb66fdf837d708bac19e99b9a994903bb830
Vivek Chaudhary <vkc369258@gmail.com>
2023-09-04 14:32:16 +0000

    Update scripts in database.sql

--- server/src/config/database.sql
+++ server/src/config/database.sql
@@ -4,3 +4,10 @@ CREATE TABLE todo_lists(
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(255)
 );
+
+INSERT INTO todo_lists(description) 
+VALUES 
+    ('Get eggs from market.'), 
+    ('Get WFH approvals from manager.'), 
+    ('Fix laptop by visiting showroom.'), 
+    ('Complete development of PERN-TODO project.');
