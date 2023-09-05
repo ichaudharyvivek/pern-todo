df20af3eca847b30d2c8557c8739eaeee54744a2
Vivek Chaudhary <vkc369258@gmail.com>
2023-09-05 13:01:18 +0000

    Add list todos functionality

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,71 @@
+import React, { Fragment, useState, useEffect } from 'react';
+
+const ListTodos = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_ENDPOINT}/todo`
+      );
+
+      const { data } = await response.json();
+
+      setTodoList(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const deleteTodo = async (todo_id) => {
+    try {
+      const deletedTodo = await fetch(
+        `${process.env.REACT_APP_API_ENDPOINT}/todo/${todo_id}`,
+        {
+          method: 'DELETE',
+        }
+      );
+
+      // Reset the todo lists
+      setTodoList(todoList.filter((todo) => todo.todo_id !== todo_id));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Fragment>
+      <table className='table table-bordered w-75 ml-auto mr-auto mt-5'>
+        <thead>
+          <tr>
+            <th className='w-75'>Description</th>
+            <th className='w-25'>Edit</th>
+            <th className='w-25'>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todoList.map((todo) => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td>edit</td>
+              <td>
+                <button
+                  className='btn btn-danger'
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
